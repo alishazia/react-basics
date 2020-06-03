8a6b1fb5c691ed4e8d8ad76ecd0cdd1f4d4d421c
alishazia <shaziaali812@gmail.com>
2020-06-03 18:19:19 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-gtrn4t
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-gtrn4t)
---
+++ app.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+
+
+class App extends Component {
+  constructor(){
+    super()
+    this.state={
+
+    }
+  }
+
+  render(){
+    return(
+      <div>hello, creating a new project</div>
+    )
+  }
+}
